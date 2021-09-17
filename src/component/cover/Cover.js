58b0d2e38560519b68fb55c821bce7236edeb5a1
Negro <fabian_1244@hotmail.com>
2021-09-16 21:42:09 -0300

    Feature: imagenes y fondo agregados

--- src/component/cover/Cover.js
+++ src/component/cover/Cover.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Cover.css";
-import planeta from "../../media/planeta.mp4";
+import planeta from "../../media/torta.mp4";
 import Typed from 'react-typed';
 
 const Cover = () => {
@@ -8,11 +8,11 @@ const Cover = () => {
     return (
         <div className="cover-container text-center mt-5">
             <video className="video" src={planeta} autoPlay loop muted />
-            <h1>Intelligence</h1>
+            <h1>Dulce Eliester</h1>
             <Typed className="letras"
                 strings={[
-                    'Ocikotorzido',
-                    'Fabian Astorga']}
+                    'Sigueme en instagram',
+                    '@Dulce_eliester_']}
                 typeSpeed={50}
                 backSpeed={50}
                 loop>
--- src/component/proyectos/Proyectos.js
+++ src/component/proyectos/Proyectos.js
@@ -1,56 +1,69 @@
 import React from 'react'
 import "./Proyectos.css";
-import prueba from "../../media/prueba.jpg";
-import git from "../../media/github.png";
-import fonicular from "../../media/fonicular.png";
-import congenial from "../../media/congenial.jpg"
-import facial from "../../media/facial.jpg"
-import neural from "../../media/neural.jpg"
+import pastel from "../../media/pastel.jpg";
+
 import { Link, Redirect } from 'react-router-dom';
 
 
 const Proyectos = () => {
 
     return (
-        <div className="container p-5">
-            <div className="row">
-                <div className="col-lg-4 text-center">
-                    <h1 className="text-dark">Titulo</h1>
-                    <p>Hola</p>
-                    <img className="img-fluid" src={congenial}/>
-                    <a href="https://github.com/Ocikotorzido/Web-page">
-                        <img className="bloque img-fluid logo mt-2" src={git}/>    
-                    </a>                  
+        <div class="row  ">
+            <div className="col-lg-4 p-5">
+                <div class="card">
+                    <img src={pastel} class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>            </div>
                 </div>
-
-                <div className=" col-lg-4 text-center">
-                    <h1 className="text-dark">Titulo</h1>
-                    <p>Hola</p>
-                    <img className="img-fluid" src={facial}/>
-                    <a href="https://github.com/Ocikotorzido/Face">
-                        <img className="bloque img-fluid logo mt-2" src={git}/>    
-                    </a>           
-                </div>
-                    
-                <div className="col-lg-4 text-center">
-                    <h1 className="text-dark">Titulo</h1>
-                    <p>Hola</p>
-                    <img className="img-fluid" src={neural}/>
-                    <a href="https://github.com/Ocikotorzido/IA">
-                        <img className="bloque img-fluid logo mt-2" src={git}/>    
-                    </a>                  
-                </div>
-                <div className="col-lg-4 text-center mt-5"></div>
-                <div className="col-lg-4 text-center mt-5">
-                    <h1 className="text-dark">Titulo</h1>
-                    <p>Hola</p>
-                    <img className="img-fluid" src={prueba}/>
-                    <Link to="">
-                        <img className="bloque img-fluid logo mt-2" src={git}/>    
-                    </Link>       
-                </div>
-                <div className="col-lg-4 text-center mt-5"></div>
             </div>
+            <div className="col-lg-4 p-5">
+                <div class="card">
+                    <img src={pastel} class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>            </div>
+                </div>
+            </div>
+            <div className="col-lg-4 p-5">
+                <div class="card">
+                    <img src={pastel} class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>            </div>
+                </div>
+            </div>
+            <div className="col-lg-4 p-5">
+                <div class="card">
+                    <img src={pastel} class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>            </div>
+                </div>
+            </div>
+            <div className="col-lg-4 p-5">
+                <div class="card">
+                    <img src={pastel} class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>            </div>
+                </div>
+            </div>
+            <div className="col-lg-4 p-5">
+                <div class="card">
+                    <img src={pastel} class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>            </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
